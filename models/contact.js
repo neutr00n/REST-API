9c1a669be62bf229cf542c9c33aebc2f82c153f5
Pavel S <neutr00n90@gmail.com>
2023-06-06 10:40:31 +0300

    Added  versionKey

--- models/contact.js
+++ models/contact.js
@@ -1,22 +1,25 @@
 const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false }
+);
 
 contactSchema.post("save", handleMongooseError);
 const Contact = model("contact", contactSchema);
